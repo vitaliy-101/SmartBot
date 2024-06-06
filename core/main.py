ff1ccd95ad466c0ad46c8954b7c50e0c58f1fe42
v.kostin2 <vitaliykostin2004@mail.ru>
2024-06-06 21:29:11 +0300

    Initial commit

---
+++ core/main.py
@@ -0,0 +1,21 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from core.Settings import settings
+import logging
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s - [%(levelname)s] - %(name)s - "
+                               "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s")
+
+    bot = Bot(settings.bots.bot_token)
+    bot.default.parse_mode = 'HTML'
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
